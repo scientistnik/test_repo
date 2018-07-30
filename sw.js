2ff2045bed20dded22a56cdbf4109b9877ed2d5a
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@karma.red>
2018-07-30 19:02:38 +0300

    add install event

---
+++ sw.js
@@ -0,0 +1,12 @@
+var CACHE_NAME = 'my-site-cache-v1';
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(['/main.js']);
+      })
+  );
+});
